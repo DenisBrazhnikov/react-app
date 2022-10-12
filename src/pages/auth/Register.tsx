17708063755e1d9cc122a1c1f3037d8ce60e5792
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-10-12 21:53:51 +0300

    implement user registration

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser, faAt, faKey, faRightToBracket} from "@fortawesome/free-solid-svg-icons";
+import {AuthContext} from '../../auth/auth-context';
+import {useForm} from "../../utility/hooks";
+import {useMutation, gql} from "@apollo/client";
+import {useNavigate} from "react-router-dom";
+
+const REGISTER_USER = gql`
+    mutation Mutation(
+        $registerInput: RegisterInput!
+    ) {
+        register(
+            registerInput: $registerInput
+        ) {
+            login
+        }
+    }
+`
 
 function Register() {
+    const context = useContext(AuthContext);
+    let navigate = useNavigate();
+    const [errors, setErrors] = useState<any>([]);
+
+    function registerUserCallback() {
+        registerUser().then();
+    }
+
+    const {onChange, onSubmit, values} = useForm(registerUserCallback, {
+        login: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    })
+
+    const [registerUser, {loading}] = useMutation(REGISTER_USER, {
+        update(proxy, { data: { registerInput: userData}}) {
+            console.log('update1'); // <-- execution exceeds the code
+            context.login(userData);
+            console.log('update2'); // <-- doesn't
+            navigate('/');
+        },
+        onError({graphQLErrors}) {
+            setErrors(graphQLErrors);
+        },
+        variables: { registerInput: values }
+    });
+
     return (
         <>
             <div className="col-4 m-auto mt-5">
@@ -19,13 +63,13 @@ function Register() {
                         <span className="input-group-text text-muted">
                             <FontAwesomeIcon icon={faUser} />
                         </span>
-                        <input type="email" className="form-control" placeholder="Neo"/>
+                        <input type="text" name="login" onChange={onChange} className="form-control" placeholder="Neo"/>
                     </div>
                     <div className="input-group mb-3">
                         <span className="input-group-text text-muted">
                             <FontAwesomeIcon icon={faAt} />
                         </span>
-                        <input type="email" className="form-control" placeholder="mr@andreson.com"/>
+                        <input type="email" name="email" onChange={onChange} className="form-control" placeholder="mr@andreson.com"/>
                     </div>
                     <div className="input-group mb-3">
                         <span className="input-group-text text-muted">
@@ -40,7 +84,7 @@ function Register() {
                         <input type="password" className="form-control" placeholder="Confirm password"/>
                     </div>
 
-                    <button className="w-100 btn btn-primary fw-semibold" type="submit">
+                    <button className="w-100 btn btn-primary fw-semibold" type="submit" onClick={onSubmit}>
                         <FontAwesomeIcon icon={faRightToBracket} className="me-2"/>
                         Register
                     </button>
