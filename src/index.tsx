b05f5acf275e6855408d1ece091923f043ffef69
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-10-12 21:53:31 +0300

    use auth provider for App component

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,40 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import {AuthProvider} from "./auth/auth-context";
+import {setContext} from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:5000/',
+});
+
+const authLink = setContext((_,{header}) => {
+    return {
+        headers: {
+            ...header,
+            authorization: localStorage.getItem('token') || ''
+        }
+    }
+});
 
 const client = new ApolloClient({
-    uri: 'http://localhost:5000/',
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
 });
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <ApolloProvider client={client}>
-              <App />
-          </ApolloProvider>
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <AuthProvider>
+            <BrowserRouter>
+                <ApolloProvider client={client}>
+                    <App/>
+                </ApolloProvider>
+            </BrowserRouter>
+        </AuthProvider>
+    </React.StrictMode>
 );
